7f1e83ee7b1c827ee05296dd1179222cb7f3e7d5
Casper Bakker <casper@picqer.com>
2023-09-20 10:32:40 +0200

    Add parcel statuses

---
+++ src/Picqer/Carriers/SendCloud/ParcelStatus.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Picqer\Carriers\SendCloud;
+
+/**
+ * @property integer id
+ * @property string message
+ */
+class ParcelStatus extends Model
+{
+    use Query\Findable;
+
+    protected $fillable = [
+        'id',
+        'message'
+    ];
+
+    protected $url = 'parcels/statuses';
+
+    protected $namespaces = [
+        'singular' => 'parcel_status',
+        'plural' => 'parcel_statuses'
+    ];
+
+}
